1b7030234409526074fde5ca99f902a3b1756d33
Asaf Saenz <asaf@saenz.mx>
2019-02-26 16:37:22 -0600

    Angular material sidenav and toolbar added

---
+++ src/app/ui/sidebar/sidebar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {} from '@angular/material';
+import { MatDrawerContainer, MatDrawer} from '@angular/material';
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+  events: string[] = [];
+  opened: boolean;
+  shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
